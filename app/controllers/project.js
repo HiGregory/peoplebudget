744cb283d6ab075f8935934910e0c723106a2ae9
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-07-15 11:55:02 -0400

    Finished /api/project/:id route

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -5,11 +5,29 @@ router.get('/', async (req, res, next) => {
     let results = null;
     try {
         results = await Project.findAll();
-        res.statusCode(200);
+        res.statusCode = 200;
         res.send({ respond: results });
-    } catch(error) {
+    } catch (error) {
         next(error);
     }
 });
+
+router.get('/:id', async (req, res, next) => {
+    let results = null;
+    const id = req.params.id;
+    try {
+        results = await Project.findOne({ where: { id: id } });
+        if (results === null) {
+            res.statusCode = 404;
+            res.send('Not found project with id: ' + id);
+        } else {
+            res.statusCode = 200;
+            res.send({ respond: results })
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = { router, version: 1 };
