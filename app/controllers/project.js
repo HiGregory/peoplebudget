0f556bbec6e76a4d0e0d85185b50ef4fe2bf0213
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-07-15 12:15:24 -0400

    Finished POST /api/project route

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Project = require('../models/project');
+const {customValidator} = require("../helpers/validator");
 
 router.get('/', async (req, res, next) => {
     let results = null;
@@ -22,12 +23,44 @@ router.get('/:id', async (req, res, next) => {
             res.send('Not found project with id: ' + id);
         } else {
             res.statusCode = 200;
-            res.send({ respond: results })
+            res.send({ respond: results });
         }
     } catch (error) {
         next(error);
     }
 });
 
+router.post('/', async (req, res, next) => {
+    const validationError = customValidator(req.body, {
+        projectId: { type: 'numeric' },
+        title: null,
+        author: null,
+        description: null,
+        votes: { type: 'numeric' },
+        avatar: null,
+        details: null
+    });
+    if (validationError) {
+        next(validationError);
+        return;
+    }
+    const { projectId, title, author, description, votes, avatar, details } = req.body;
+    try {
+        const project = await Project.create({
+            projectId,
+            title,
+            author,
+            description,
+            votes,
+            avatar,
+            details
+        });
+        res.header('Location', `api/v1/project/${project.projectId}`);
+        res.statusCode = 201;
+        res.send({ responsd: 'Project created' });
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = { router, version: 1 };
