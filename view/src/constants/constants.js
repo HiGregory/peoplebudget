8a2802651619fe727a0a246f6d01a3a691f056a1
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-07-18 10:28:19 -0400

    updated google map API keys

--- view/src/constants/constants.js
+++ view/src/constants/constants.js
@@ -3,4 +3,4 @@ export const PORT = 7000;
 export const BASE_API_URL = '/api/v1/auth'
 export const PROTOCOL = 'http://'
 export const SIGNUP_ENDPOINT = '/register'
-export const MAP_KEY= 'AIzaSyCWWMOQwBoYitILv-_Gd_dd9BuhiCiwqFk'
+export const MAP_KEY= 'AIzaSyCKTpPPRwKTvjAf4hl_pvHL5NSOHTvA1Io'
